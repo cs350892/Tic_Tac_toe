9bcbccb5d9ff629823825df62cee8e01de06259b
cs350892 <104549787+cs350892@users.noreply.github.com>
2023-02-12 22:54:39 +0530

    Add files via upload

---
+++ ChandraProject TicTacToe/myscript.js
@@ -0,0 +1,60 @@
+console.log("This is my script");
+let music=new Audio("music.mp3");
+let ting=new Audio("ting.mp3");
+let gameover=new Audio("gameover.mp3");
+let turn="X";
+let isgameover=false;
+
+const changeTurn=()=>{
+//chanaage karne ka logic likha jaayega
+return turn==="X"?"0":"X";
+}
+
+//Check Winning Of the GAme
+const checkWin=()=>{
+let boxtext=document.getElementsByClassName('boxtext');
+let wins=[
+[0,1,2,5,5,0],
+[3,4,5,5,15,0],
+[6,7,8,5,25,0],
+[0, 3, 6, -5, 15, 90],
+[1, 4, 7, 5, 15, 90],
+[2, 5, 8, 15, 15, 90],
+[0, 4, 8, 5, 15, 45],
+[2, 4, 6, 5, 15, 135]
+]
+wins.forEach(e=>{
+    if(boxtext[e[0]].innerHTML==boxtext[e[1]]&&boxtext[e[1]].innerHTML==boxtext[e[2]]&&boxtext[[e[0]]].innerHTML==''){
+        document.querySelector('.info').innerHTML=boxtext[e[0]] + "Won";
+        isgameover="true";
+        document.querySelector('.imgbox').getElementsByTagName('.img')[0].style.width="220px";
+        document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+        document.querySelector(".line").style.width = "20vw";
+    }
+
+})
+
+}
+
+
+//gameLogic
+let boxes=document.getElementsByClassName("box");
+Array.from(boxes).forEach(element=>{
+let boxtext=element.querySelector(".boxtext");
+element.addEventListener('click',()=>{
+if(boxtext.innerText===''){
+    boxtext.innerText=turn;
+    turn=changeTurn();
+    ting.play();
+checkWin();
+if(!isgameover){
+    document.getElementsByClassName(".info")[0].innerText="turn for" +turn;
+
+}
+}
+
+});
+
+
+
+});
